4bea67d69d218a9368a39cdc65b5c826bacaff2c
Abhay Gupta <ab04012003@gmail.com>
2024-09-11 10:45:43 +0530

    hello world program code of java class file is added

---
+++ _01_Variable_&_DataTypes/Variables/_03hello_world.java
@@ -0,0 +1,9 @@
+// print Hello world in java
+public class _03hello_world {
+    public static void main (String args []){
+
+        // code to print the Hello world!
+        System.out.println("Hello world!");
+        
+    }
+}
