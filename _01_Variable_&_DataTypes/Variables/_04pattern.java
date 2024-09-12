aa3e37266a40fe3a02a432adf08400279a3e2fd7
Abhay Gupta <ab04012003@gmail.com>
2024-09-12 09:45:33 +0530

    pattern program code of java file is added

---
+++ _01_Variable_&_DataTypes/Variables/_04pattern.java
@@ -0,0 +1,9 @@
+public class _04pattern {
+    public static void main(String args[]){
+        // code to print pattern 
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
