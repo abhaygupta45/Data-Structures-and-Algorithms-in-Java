fbe4139a5dca7f256af03cb110e85575effd9195
Abhay Gupta <ab04012003@gmail.com>
2024-09-12 09:40:32 +0530

    first variable code of java file is added

---
+++ _01_Variable_&_DataTypes/Variables/_05Varibles.java
@@ -0,0 +1,12 @@
+public class _05Varibles {
+    public static void main(String[] args) {
+        int a = 5; // a is a varible and int is datatype
+        float b = 0.12f; // b is a variable and float is a datatype
+        char c = 'a';
+
+        System.out.println(a); // output 
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
