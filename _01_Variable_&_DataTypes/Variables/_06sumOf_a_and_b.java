bde5edab9cdb79f7424d6f378f0c5a42ff176f02
Abhay Gupta <ab04012003@gmail.com>
2024-09-13 09:45:32 +0530

    sum of two numbers code of java file is added

---
+++ _01_Variable_&_DataTypes/Variables/_06sumOf_a_and_b.java
@@ -0,0 +1,10 @@
+public class _06sumOf_a_and_b {
+    public static void main(String args[]){
+        // code to calculate the sum of two numbers 
+        int a = 10;
+        int b = 5;
+        int sum;
+        sum = a+b;
+        System.out.println(sum);
+    }
+}
