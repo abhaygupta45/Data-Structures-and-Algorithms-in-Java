9cc8ed3935b52cc7b9691e6fb6abc39ef4a4a6b1
Abhay Gupta <ab04012003@gmail.com>
2024-09-14 09:36:56 +0530

    numberic data type code of java file is added

---
+++ _01_Variable_&_DataTypes/Variables/_08numeric_DataTypes.java
@@ -0,0 +1,21 @@
+public class _08numeric_DataTypes {
+
+    public static void main (String []args){
+
+        // byte type
+        byte num = 10;
+        System.out.println("byte data type" + num);
+
+        //short type 
+        short num1 = 20;
+        System.out.println("short data type "+ num1);
+
+        //int type
+        int num2 = 20;
+        System.out.println("int data type" +num2);
+
+        //long data type
+        long num3 = 40L;
+        System.out.println("long data type"+num3);
+    }
+}
