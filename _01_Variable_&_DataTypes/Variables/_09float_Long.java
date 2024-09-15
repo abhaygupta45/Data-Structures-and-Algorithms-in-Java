266db5a5a973b907417416fd246f34f6a6c69184
Abhay Gupta <ab04012003@gmail.com>
2024-09-15 09:22:43 +0530

    float long code of java file is added

---
+++ _01_Variable_&_DataTypes/Variables/_09float_Long.java
@@ -0,0 +1,13 @@
+public class _09float_Long {
+    
+    public static void main(String[] args)
+    {
+        // float type
+        float f = 20.25f;
+        System.out.println("f= " + f);
+
+        // double type
+        double d = 20.25;
+        System.out.println("d= " + d);
+    }
+}
