89c38c3dd994545ac7ee4578d637c4e6cc8741b0
Abhay Gupta <ab04012003@gmail.com>
2024-09-17 02:11:27 +0530

    boolean file of java is added

---
+++ _01_Variable_&_DataTypes/Variables/_11boolean.java
@@ -0,0 +1,12 @@
+public class _11boolean {
+    
+    public static void main(String[] args) {
+
+        boolean t = true;
+        System.out.println(t);
+ 
+        boolean f = false;
+        System.out.println(f);
+ 
+    }
+}
