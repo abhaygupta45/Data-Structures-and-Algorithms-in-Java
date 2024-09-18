ba2d67dd5ca6f62e6dab78bcbd56120b401b8b67
Abhay Gupta <ab04012003@gmail.com>
2024-09-19 02:04:05 +0530

    local variables of java file is added

---
+++ _01_Variable_&_DataTypes/Variables/_13local_variable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class _13local_variable {
+    
+    public static void main(String[] args)
+    {
+        // Declared a Local Variable
+        int var = 10;
+
+        // This variable is local to this main method only
+        System.out.println("Local Variable: " + var);
+    }
+}
