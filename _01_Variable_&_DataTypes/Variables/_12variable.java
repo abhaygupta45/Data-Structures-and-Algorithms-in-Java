bcacec6ddd7dd16ec2e139f4b044260c7675cdba
Abhay Gupta <ab04012003@gmail.com>
2024-09-18 15:50:06 +0530

    variable of java is added

---
+++ _01_Variable_&_DataTypes/Variables/_12variable.java
@@ -0,0 +1,15 @@
+public class _12variable {
+    
+    public static void main(String[] args) {
+ 
+        // variable declaration
+        int number;    
+     
+        // initialization / assign a value to variable
+        number = 10;
+ 
+        // modify variable value and assign it again
+        number = number + 20;
+    }
+ 
+}
