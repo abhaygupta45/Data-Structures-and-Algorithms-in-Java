23da6e8c8981c5e877762ca1dd504a93e85b0d63
Abhay Gupta <ab04012003@gmail.com>
2024-09-21 11:53:57 +0530

    java file of byte data is added

---
+++ _01_Variable_&_DataTypes/Data types/_02byteDataType.java
@@ -0,0 +1,11 @@
+public class _02byteDataType {
+    public static void main(String[] args) {
+        //allowed
+        byte data = 45;
+  System.out.println(data);
+//   Stores whole numbers from -128 to 127
+//    not allowed because the range of byte 
+//    byte data2 = 128;
+//    System.out.println(data2);
+   }
+}
