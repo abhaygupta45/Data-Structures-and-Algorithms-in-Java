9d8dbc7dc33270bc5690d697c410744c7bdc1aa3
Abhay Gupta <ab04012003@gmail.com>
2024-09-21 10:12:24 +0530

    java file of short data types is added

---
+++ _01_Variable_&_DataTypes/Data types/_03shortDataType.java
@@ -0,0 +1,9 @@
+public class _03shortDataType {
+    public static void main (String args[]){
+
+    // Stores whole numbers from -32,768 to 32,767
+    short data = 128;
+    System.out.println(data);
+    }
+}
+
