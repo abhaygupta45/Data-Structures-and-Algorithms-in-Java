ff6c75bde08508619a50c20f661a8d205ce95617
Abhay Gupta <ab04012003@gmail.com>
2024-09-22 11:53:14 +0530

    java file of long data types is added

---
+++ _01_Variable_&_DataTypes/Data types/_05longDataType.java
@@ -0,0 +1,10 @@
+public class _05longDataType {
+    
+    public static void main(String[] args) {
+        
+        // long	8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        long number = 2344l;
+        System.out.println(number);
+    }
+}
