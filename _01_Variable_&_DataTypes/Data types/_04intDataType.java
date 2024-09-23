43bdea97245b952484b24ce84c0a6247e846dae2
Abhay Gupta <ab04012003@gmail.com>
2024-09-23 10:16:12 +0530

    java file of int data types is added

---
+++ _01_Variable_&_DataTypes/Data types/_04intDataType.java
@@ -0,0 +1,7 @@
+public class _04intDataType {
+    public static void main(String[] args) {
+        // 4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        int a = 10;
+        System.out.println(a);
+    }
+}
