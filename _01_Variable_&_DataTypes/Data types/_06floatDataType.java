48843f1333fa25cce47b4601c779cf5c1bb600d8
Abhay Gupta <ab04012003@gmail.com>
2024-09-24 19:18:41 +0530

    java file of float data type is added

---
+++ _01_Variable_&_DataTypes/Data types/_06floatDataType.java
@@ -0,0 +1,10 @@
+public class _06floatDataType {
+    
+    public static void main(String[] args) {
+        
+        // float	4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+
+        float percent = 92.23f;
+        System.out.println(percent);
+    }
+}
