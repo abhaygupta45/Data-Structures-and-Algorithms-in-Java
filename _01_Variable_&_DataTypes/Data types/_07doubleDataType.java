8d94f108d4d6926bbe434ebf3a7441be87efacbe
Abhay Gupta <ab04012003@gmail.com>
2024-09-25 12:41:18 +0530

    java file of double data types is added

---
+++ _01_Variable_&_DataTypes/Data types/_07doubleDataType.java
@@ -0,0 +1,10 @@
+public class _07doubleDataType {
+    
+    public static void main(String[] args) {
+        
+        // double	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+
+        double num = 56.84739874973;
+        System.out.println(num);
+    }
+}
