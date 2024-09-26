063cd7895fb7bd060c9bee31db0e7e3f6f6913a0
Abhay Gupta <ab04012003@gmail.com>
2024-09-26 10:09:35 +0530

    java file of boolean data types is added

---
+++ _01_Variable_&_DataTypes/Data types/_08booleanDataTypes.java
@@ -0,0 +1,13 @@
+public class _08booleanDataTypes {
+    
+    public static void main(String[] args) {
+        
+        // boolean	1 bit	Stores true or false values
+
+        boolean boys = true;
+        boolean girls = false;
+
+        System.out.println("boys are always : " + boys);
+        System.out.println("girls are always : " + girls);
+    }
+}
