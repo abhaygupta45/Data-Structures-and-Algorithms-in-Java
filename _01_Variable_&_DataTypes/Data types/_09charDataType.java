f728b1ec57377a7d2f6aeaacd0a11c52fc8c17f9
Abhay Gupta <ab04012003@gmail.com>
2024-09-27 10:38:11 +0530

    java file of char data types is added

---
+++ _01_Variable_&_DataTypes/Data types/_09charDataType.java
@@ -0,0 +1,10 @@
+public class _09charDataType {
+    
+    public static void main(String[] args) {
+        
+        // char	2 bytes	Stores a single character/letter or ASCII values
+
+        char star = '*';
+        System.out.println(star);
+    }
+}
