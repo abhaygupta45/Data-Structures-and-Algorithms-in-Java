8971b85f9d7f8cc177bb7e873653b4cec3ce7cfc
Abhay Gupta <ab04012003@gmail.com>
2024-09-28 09:21:11 +0530

    java file of Type casting program is added

---
+++ _01_Variable_&_DataTypes/TypeCasting/_01program.java
@@ -0,0 +1,16 @@
+// Type casting is when you assign a value of one primitive data type to another type.
+
+// byte -> short -> char -> int -> long -> float -> double
+
+
+public class _01program {
+    
+    public static void main(String[] args) {
+        
+        int a = 45;
+        double b;
+        b = a ;
+        System.out.println(b);
+    }
+}
+
