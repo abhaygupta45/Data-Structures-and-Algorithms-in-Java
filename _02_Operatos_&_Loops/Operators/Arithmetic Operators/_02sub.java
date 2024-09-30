31656af7f40b455303b1e1fe686a4c1654fb83f0
Abhay Gupta <ab04012003@gmail.com>
2024-09-30 10:55:18 +0530

    java file of add Operator is added

---
+++ _02_Operatos_&_Loops/Operators/Arithmetic Operators/_02sub.java
@@ -0,0 +1,11 @@
+public class _02sub {
+    
+    public static void main (String args[]){
+    
+        int num1 = 20;
+        int num2 = 10;
+        int sub = num1 - num2;
+    
+        System.out.println("sub of num1 and num2 is "+ sub);
+        }
+}
