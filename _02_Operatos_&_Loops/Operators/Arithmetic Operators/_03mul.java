03d2c7262b1511af8ac99b14bb9b9acc86650ff2
Abhay Gupta <ab04012003@gmail.com>
2024-10-01 19:51:23 +0530

    java file of mul arthematic operator is added

---
+++ _02_Operatos_&_Loops/Operators/Arithmetic Operators/_03mul.java
@@ -0,0 +1,10 @@
+public class _03mul {
+    public static void main (String args[]){
+    
+        int num1 = 5;
+        int num2 = 10;
+        int mul = num1 * num2;
+    
+        System.out.println("multipe of num1 and num2 is "+ mul);
+        }
+}
