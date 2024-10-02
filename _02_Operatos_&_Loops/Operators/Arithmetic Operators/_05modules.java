254c96ef176aa13d158462c94b3a9052b12f2570
Abhay Gupta <ab04012003@gmail.com>
2024-10-03 01:14:52 +0530

    java file of modulus Operator is added

---
+++ _02_Operatos_&_Loops/Operators/Arithmetic Operators/_05modules.java
@@ -0,0 +1,11 @@
+public class _05modules {
+    
+    public static void main (String args[]){
+    
+        int num1 = 20;
+        int num2 = 6;
+        int modules = num1 % num2;
+    
+        System.out.println("reminder of num1 and num2 is "+ modules);
+        }
+}
