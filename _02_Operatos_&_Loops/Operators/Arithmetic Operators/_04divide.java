b3c24e841c6d6312c78ee13d537558fbd9f49850
Abhay Gupta <ab04012003@gmail.com>
2024-10-02 11:25:34 +0530

    java file of divide operator is added

---
+++ _02_Operatos_&_Loops/Operators/Arithmetic Operators/_04divide.java
@@ -0,0 +1,11 @@
+public class _04divide {
+    
+    public static void main (String args[]){
+    
+        int num1 = 30;
+        int num2 = 10;
+        int divide = num1 / num2;
+    
+        System.out.println("divide of num1 and num2 is "+ divide);
+        }
+}
