f831b7f43ab3e5454370f738d07f3d221cd7159e
Abhay Gupta <ab04012003@gmail.com>
2024-10-03 10:43:44 +0530

    java file of tricy question using double

---
+++ _02_Operatos_&_Loops/Operators/Arithmetic Operators/_06tricyQuestionUsingDouble.java
@@ -0,0 +1,13 @@
+    public class _06tricyQuestionUsingDouble {
+
+            public static void main(String args[])
+            {
+                double var1 = 1 + 5; 
+                double var2 = var1 / 4; // here the data type is double that why in divide process the exact value is get. 6.0 / 4 
+                System.out.println(var2);
+                int var3 = 1 + 5;
+                int var4 = var3 / 4;
+                System.out.print(var4);
+    
+            } 
+        }
