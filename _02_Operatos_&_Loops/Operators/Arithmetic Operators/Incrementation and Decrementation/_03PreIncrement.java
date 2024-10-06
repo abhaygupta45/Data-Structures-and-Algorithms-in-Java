2df0ccea0dbc1b91d0f596d7923d235b9e665978
Abhay Gupta <ab04012003@gmail.com>
2024-10-06 10:23:45 +0530

    java file Pre increment is added

---
+++ _02_Operatos_&_Loops/Operators/Arithmetic Operators/Incrementation and Decrementation/_03PreIncrement.java
@@ -0,0 +1,10 @@
+public class _03PreIncrement {
+    
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
