112e578cbbd16d4d9a98d0c079e968e5de583cc6
Abhay Gupta <ab04012003@gmail.com>
2024-10-10 09:47:54 +0530

    JAVA file of program 2 is added

---
+++ _02_Operatos_&_Loops/Operators/Conditionals/_02program.java
@@ -0,0 +1,13 @@
+public class _02program {
+    
+    public static void main(String[] args) {
+        
+        int age = 18;
+        
+        if(age >= 18){
+            System.out.println("Age is greater than or equal to 18");
+        }else {
+            System.out.println("age is less than 18");
+        }
+    }
+}
