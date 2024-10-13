ee901bf97c7195714ae357f59e8759f45396a69f
Abhay Gupta <ab04012003@gmail.com>
2024-10-13 10:10:13 +0530

    java file of logical operators is added

---
+++ _02_Operatos_&_Loops/Operators/Logical Operators/LogicalOperators.java
@@ -0,0 +1,20 @@
+
+// You can also test for true or false values with logical operators.
+
+// Logical operators are used to determine the logic between variables or values:
+
+public class LogicalOperators {
+
+    public static void main (String[] args){
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println(a==b && a<b && a>c);
+        System.out.println(a>b || a<c || a>=b);
+        System.out.println(!true);
+        System.out.println(!false);
+    }
+    
+}
