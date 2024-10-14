c289b4421eaed08ef40bed6205b7943bdc20f084
Abhay Gupta <ab04012003@gmail.com>
2024-10-14 10:23:34 +0530

    Java file of Relational Operator is added

---
+++ _02_Operatos_&_Loops/Operators/Relational Operators/_02RelationalOperators.java
@@ -0,0 +1,13 @@
+public class _02RelationalOperators {
+    public static void main (String[] args){
+        int a = 10;
+        int b = 20;
+        boolean result = a == b;
+        boolean result1 = a >= b;
+        boolean result2 = a <= b;
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+    
+}
