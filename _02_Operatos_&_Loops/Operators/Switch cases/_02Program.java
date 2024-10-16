726619c3d2415468caf534434f13a786006294c8
Abhay Gupta <ab04012003@gmail.com>
2024-10-16 10:25:12 +0530

    javafile of Program 2 switch cases is added

---
+++ _02_Operatos_&_Loops/Operators/Switch cases/_02Program.java
@@ -0,0 +1,14 @@
+public class _02Program {
+    
+    public static void main(String[] args) {
+        
+        int num = 200;
+
+        switch(num){
+            case 200: System.out.println("case1");
+            case 100: System.out.println("case2");
+            case 300: System.out.println("case3");
+            default : System.out.println("default");
+        }
+    }
+}
