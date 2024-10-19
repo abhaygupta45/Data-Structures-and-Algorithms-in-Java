a38778b9a642fc387c72acebac65b9585176c0fb
Abhay Gupta <ab04012003@gmail.com>
2024-10-19 11:37:40 +0530

    java file of program 2 ternory operator is added

---
+++ _02_Operatos_&_Loops/Operators/Ternary Operator/_02Program.java
@@ -0,0 +1,11 @@
+public class _02Program {
+    
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=20;
+
+        int res = (a<b)?a:b;
+        System.out.println(res);
+    }
+}
