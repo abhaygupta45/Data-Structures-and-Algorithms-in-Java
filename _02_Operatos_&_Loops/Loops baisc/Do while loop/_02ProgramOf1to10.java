d268ed4a1778f33c4a96e1ecfddc91125135ab6b
Abhay Gupta <ab04012003@gmail.com>
2024-10-23 09:06:40 +0530

    java fileof program 1 to 10 is added

---
+++ _02_Operatos_&_Loops/Loops baisc/Do while loop/_02ProgramOf1to10.java
@@ -0,0 +1,12 @@
+public class _02ProgramOf1to10 {
+
+    public static void main(String[] args) {
+        int num = 1;
+
+        do {
+            System.out.println(num);
+            num++;
+        }
+        while(num <= 10);
+    }
+}
