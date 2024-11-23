8928a2d7cb16f017f8a1674729a6529c29b394d2
Abhay Gupta <ab04012003@gmail.com>
2024-11-24 04:54:44 +0530

    java file of half Square shape pattern is added

---
+++ _03_Loops_&_Pattern_Programming/_05Practice Pattern Programming/_03HalfSquarePattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class _03HalfSquarePattern{
+    public static void main (String args []){
+    
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number :-");
+        int n = scan.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0 || i==(n-1)/2 || j==n-1){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}   
