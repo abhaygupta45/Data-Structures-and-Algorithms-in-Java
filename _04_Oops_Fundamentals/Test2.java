9b79677a932e50c3ba497c267cdfbc63e79caaca
Abhay Gupta <ab04012003@gmail.com>
2024-12-03 11:23:43 +0530

    java file of test2 is added

---
+++ _04_Oops_Fundamentals/Test2.java
@@ -0,0 +1,12 @@
+class Test2 {
+    
+    int a;
+    String name;
+    public static void main(String[] args) {
+        // declare the variable
+        // create the object
+
+        int num = 9;
+        Test2 obj=new Test2();
+    }
+}
