8f62d44402bdd84e5dd5ee7ea63c00b60a9f3257
Abhay Gupta <ab04012003@gmail.com>
2024-12-06 11:43:45 +0530

    java file of first object is added

---
+++ _04_Oops_Fundamentals/_01FirstObject.java
@@ -0,0 +1,13 @@
+// it is not cumpolsory to make class name and file name similer (situation is not use public class if you use public class then it is cumpulsory to use file and class name similer)
+class _01FirstObject { 
+    int a;
+    String name;
+
+    public static void main(String[] args) {
+        // declare the variable
+        // create the object
+
+        int num = 9;
+        _01FirstObject obj = new _01FirstObject();
+    }
+}
