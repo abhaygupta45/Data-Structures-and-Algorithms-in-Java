41e2b92e3cba3fdf4c99398780cd9f04ed5bb623
Abhay Gupta <ab04012003@gmail.com>
2024-12-14 11:23:34 +0530

    creating an array file is added

---
+++ _06_Array_In_Java/_02Creating_An_Array/_01Creating1DArray.java
@@ -0,0 +1,12 @@
+public class _01Creating1DArray {
+    public static void main(String args[]){
+
+        int nums[] = {2,4,5,6,7};
+        System.out.println(nums[3]);//print the value of index number 3 which is 6
+
+        //print all values using loop
+        for (int i=0; i<5; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
