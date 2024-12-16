52ef3b0c37a7ba50605d51e66825a0415cff81e0
Abhay Gupta <ab04012003@gmail.com>
2024-12-16 10:32:54 +0530

    array method  file is added

---
+++ _06_Array_In_Java/_02Creating_An_Array/_02CreatingArray2ndMethod.java
@@ -0,0 +1,17 @@
+public class _02CreatingArray2ndMethod {
+    
+    public static void main(String[] args) {
+        
+        int nums[] = new int[4];
+
+        nums[0] = 5;
+        nums[1] = 4;
+        nums[2] = 3;
+        nums[3] = 2;
+
+        for(int i=0; i<=3; i++){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
