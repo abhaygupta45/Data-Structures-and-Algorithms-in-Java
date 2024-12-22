8dde888332aacf709da8b906018436677eccf7cb
Abhay Gupta <ab04012003@gmail.com>
2024-12-22 09:11:43 +0530

    2 D array method file is added

---
+++ _06_Array_In_Java/_03_2D_Array/_02Creating2D_Array_2nd_method.java
@@ -0,0 +1,18 @@
+public class _02Creating2D_Array_2nd_method {
+    
+    public static void main(String[] args) {
+        
+        int nums [] [] = {
+                            {6,5},
+                            {4,3},
+                            {2,1}
+                         };
+
+        for (int i=0; i<=2; i++){
+            for(int j=0; j<=1; j++){
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
