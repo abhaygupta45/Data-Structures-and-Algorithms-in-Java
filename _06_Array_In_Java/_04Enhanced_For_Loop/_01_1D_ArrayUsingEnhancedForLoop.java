c5b04598e867467c15db085b9aff1535fba52a0a
Abhay Gupta <ab04012003@gmail.com>
2024-12-27 10:29:43 +0530

    Array using enhanced for loop file is added

---
+++ _06_Array_In_Java/_04Enhanced_For_Loop/_01_1D_ArrayUsingEnhancedForLoop.java
@@ -0,0 +1,11 @@
+public class _01_1D_ArrayUsingEnhancedForLoop {
+
+    public static void main(String[] args) {
+        
+        int nums[] = {1,2,3,4};
+
+        for(int n:nums){
+            System.out.println(n);
+        }
+    }
+}
