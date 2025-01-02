e6420efd698e83c397f50a1fb4a7c8bb8a7e21e4
Abhay Gupta <ab04012003@gmail.com>
2025-01-02 08:12:57 +0530

    java file of length property 2 D array is added

---
+++ _06_Array_In_Java/_06Length_vs_Length()/Practice/Program02LengthPropertyI2D_Array.java
@@ -0,0 +1,13 @@
+public class Program02LengthPropertyI2D_Array {
+    
+    public static void main (String args[]){
+    
+        int nums [] [] = {
+                            {3,4,5,6},
+                            {3,4,5,6},
+                            {3,4,5,6}
+                         };
+        System.out.println(nums.length); // using the length property to find the length of the array
+
+    }
+}
