68957a6891951466b588a89f52668f71b49cb7e2
Abhay Gupta <ab04012003@gmail.com>
2025-01-15 09:15:16 +0530

    java file of practice program is added

---
+++ _06_Array_In_Java/_04Enhanced_For_Loop/Practice/Program01SingleDimensional.java
@@ -0,0 +1,11 @@
+public class Program01SingleDimensional {
+    
+    public static void main(String[] args) {
+        
+        int nums [] = {2,3,4,5};
+
+        for(int n:nums){
+            System.out.println(n);
+        }
+    }
+}
