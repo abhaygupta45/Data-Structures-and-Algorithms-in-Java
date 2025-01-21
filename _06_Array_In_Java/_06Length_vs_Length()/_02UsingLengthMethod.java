3f327998a7cdc4358e72431c88c5617adbcc2693
Abhay Gupta <ab04012003@gmail.com>
2025-01-21 11:56:35 +0530

    java file of using length method is added

---
+++ _06_Array_In_Java/_06Length_vs_Length()/_02UsingLengthMethod.java
@@ -0,0 +1,10 @@
+public class _02UsingLengthMethod {
+
+    public static void main(String[] args) {
+        
+        String name = "Abhay";
+        System.out.println(name.length()); // In string length method is used while in array length property is used
+        
+    }
+    
+}
