a5a87c493d9d80269baafb199564cac6787613b6
Abhay Gupta <ab04012003@gmail.com>
2025-01-23 10:44:12 +0530

    java file of practice length propeerty is added

---
+++ _06_Array_In_Java/_06Length_vs_Length()/Practice/Program01LengthPropertyI1D_Array.java
@@ -0,0 +1,9 @@
+public class Program01LengthPropertyI1D_Array {
+
+    public static void main (String args[]){
+    
+        int nums []= {4,3,2,1};
+        System.out.println(nums.length); // using the length property to find the length of the array
+
+    }
+}
