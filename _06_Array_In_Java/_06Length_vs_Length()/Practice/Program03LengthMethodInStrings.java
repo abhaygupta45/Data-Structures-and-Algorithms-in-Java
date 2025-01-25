cf2e9aadcc9032d565a402a10a11ff6398b7ec9b
Abhay Gupta <ab04012003@gmail.com>
2025-01-25 08:15:11 +0530

    java file of length method in String is added

---
+++ _06_Array_In_Java/_06Length_vs_Length()/Practice/Program03LengthMethodInStrings.java
@@ -0,0 +1,10 @@
+public class Program03LengthMethodInStrings {
+    
+    public static void main(String[] args) {
+        
+        String names = "Abhay";
+
+        // using length method in string to find the length of the String
+        System.out.println(names.length());
+    }
+}
