d92618752da8e3f3bdda65260bfcaae55f01e9cb
Abhay Gupta <ab04012003@gmail.com>
2025-02-12 11:23:12 +0530

    java file of String Introduction Prgram 2 is added

---
+++ _07_Strings_In_Java/Part1_StringIntroduction/Practice/Program02.java
@@ -0,0 +1,13 @@
+public class Program02 {
+
+    public static void main(String[] args) {
+        
+        //First method of creating String
+        String name = "PW";
+        System.out.println(name);
+
+        // second menthod to print Immutable String
+        String name1 = new String("PW");
+        System.out.println(name1);
+    }
+}
