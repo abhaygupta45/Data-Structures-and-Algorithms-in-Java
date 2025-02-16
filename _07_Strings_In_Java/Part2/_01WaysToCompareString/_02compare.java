e1c97426e4b85b48dc09354f938174872bff0759
Abhay Gupta <ab04012003@gmail.com>
2025-02-16 12:47:24 +0530

    java file of compare string is added

---
+++ _07_Strings_In_Java/Part2/_01WaysToCompareString/_02compare.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class _02compare {
+
+    public static void main(String[] args) {
+        
+        String s1 = "pwskill";
+        String s2 = "pwskill";
+        String s3 = "pwJAVA";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+
+    }
+}
